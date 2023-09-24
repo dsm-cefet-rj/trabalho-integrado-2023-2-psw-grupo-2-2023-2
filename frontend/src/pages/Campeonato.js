16b34928526e24311a31e9c64f993d146a85bfa0
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-09-24 03:36:27 -0300

    Caminhando

--- frontend/src/pages/Campeonato.js
+++ frontend/src/pages/Campeonato.js
@@ -7,11 +7,35 @@ import { useNavigate } from "react-router-dom";
 
 function Campeonato(){
     const navigate = useNavigate();
+    // fetch("https://v3.football.api-sports.io/standings?league=71&season=2023", {
+	// "method": "GET",
+	// "headers": {
+	// 	"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+	// 	"x-rapidapi-key":"85e13ac972msh0a28d2865f77831p138879jsnbbd24a9f2d99"
+	// }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
     return(
     <div>
         <Cabecalho/>
         <div className='Fundo'>
-            <img onClick={() => navigate("/perfil")} className = "ContPerfil" src = {perfil} alt = "ImagemPerfil"/>
+           
+            {/* <div id="wg-api-football-standings"
+                data-host="api-football-v1.p.rapidapi.com"
+                data-key="85e13ac972msh0a28d2865f77831p138879jsnbbd24a9f2d99"
+                data-league="71"
+                data-team=""
+                data-season="2023"
+                data-theme="dark"
+                data-show-errors="false"
+                data-show-logos="true"
+                class="wg_loader">
+            </div> */}
         </div>
     </div>
     )
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,6 +8,19 @@ import escudo from '../assets/images/Al Mir sem fundo.png'
 
 function Home(){
     const navigate = useNavigate();
+    // fetch("https://v3.football.api-sports.io/fixtures?live=all", {
+	// "method": "GET",
+	// "headers": {
+	// 	"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+	// 	"x-rapidapi-key": "85e13ac972msh0a28d2865f77831p138879jsnbbd24a9f2d99"
+	// }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
     return(
     <div>
         <Cabecalho/>
@@ -27,15 +40,22 @@ function Home(){
                     <img onClick={() => navigate("/perfil")}  src = {perfil} alt = "ImagemPerfil"/> 
                </div>
                 
-                <div className='Campeonato'>
-                    <div onClick={() => navigate("/campeonato")}>Campeonato CEFET</div>
-                </div>
-                <div className='Jogo'>
-                    <div className='Home' onClick={() => navigate("/time")} >Al Mir<img  className = "Escudo" onClick={() => navigate("/time")}  src = {escudo} alt = "escudoTime"/>  </div> 
-                    <X size={24}  className='X'/>
-                    <div className='Away' onClick={() => navigate("/time")} >Al Mir<img  className = "Escudo" onClick={() => navigate("/time")}  src = {escudo} alt = "escudoTime"/>  </div> 
-                </div>
-          
+            {/* <div id="wg-api-football-games"
+                data-host="api-football-v1.p.rapidapi.com"
+                data-key="85e13ac972msh0a28d2865f77831p138879jsnbbd24a9f2d99"
+                data-date=""
+                data-league="71"
+                data-season="2023"
+                data-theme="dark"
+                data-refresh=""
+                data-show-toolbar="true"
+                data-show-errors="false"
+                data-show-logos="true"
+                data-modal-game="true"
+                data-modal-standings="true"
+                data-modal-show-logos="true">
+            </div> */}
+                      
             </div>    
         </div>
     </div>
