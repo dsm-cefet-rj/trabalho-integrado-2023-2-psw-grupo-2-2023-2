23ab2ba46383c7a1bbf9d54e628622b71f2216ed
Daniel Vieira Medeiros <dmedeiros1209@gmail.com>
2023-09-25 08:19:54 -0300

    Adição dados dos times das 6 ligas

--- CodigoRequisição/requisicao.py
+++ CodigoRequisição/requisicao.py
@@ -1,26 +1,26 @@
 import requests
 
-url = "https://api-football-v1.p.rapidapi.com/v3/players"
+url = "https://api-football-v1.p.rapidapi.com/v3/teams"
 
-for i in range(2,34):
-    querystring = {"league":"78","season":"2023","page":i}
 
-    headers = {
-        "X-RapidAPI-Key": "f9476b42d7mshde65990dd93c162p173969jsnc07b899065cb",
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-    }
+querystring = {"league":"78","season":"2023"}
 
-    response = requests.get(url, headers=headers, params=querystring)
+headers = {
+	"X-RapidAPI-Key": "f9476b42d7mshde65990dd93c162p173969jsnc07b899065cb",
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+}
 
-    if response.status_code == 200:
-        data = response.json()
-    else:
-        print(f"Erro na requisição: {response.status_code}")
-        data = None
+response = requests.get(url, headers=headers, params=querystring)
 
-    import json
+if response.status_code == 200:
+    data = response.json()
+else:
+    print(f"Erro na requisição: {response.status_code}")
+    data = None
 
-    if data:
-        with open("Alemanha.json", "a") as json_file:
-            json.dump(data, json_file, indent=4)
-            print("Resposta da API foi salva em api_response.json")
+import json
+
+if data:
+    with open("TimesAlemanha.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        print("Resposta da API foi salva em api_response.json")
