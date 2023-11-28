50039a276d719b376f1d01268d5d8b4df3b0e7b8
FeixeDeLuz <luiz.mendescastro@gmail.com>
2023-11-28 13:46:12 -0300

    Times Backend

--- backend/models/jogadores.js
+++ backend/models/jogadores.js
@@ -2,11 +2,53 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const normalize = requise('normalize-mongoose');
 const jogadoresSchema = new Schema({
-    nome:{
+    name:{
       type: String,
       required:true,
     },
-    sigla:{
+    id:{
+      type: int,
+      required: true,
+    },
+    firstname:{
+      type: int,
+      required: true,
+    }
+    ,
+    lastname:{
+      type: String,
+      required: true,
+    }
+    ,
+    team_id:{
+      type: int,
+      required: true,
+    }
+    team_name:{
+      type: String,
+      required: true,
+    },
+    appearences:{
+      type: int,
+      required: true,
+    },
+    position:{
+      type: String,
+      required: true,
+    },
+    rating:{
+      type: int,
+      required: true,
+    },
+    photo:{
+      type: String,
+      required: true,
+    },
+    total:{
+      type: int,
+      required: true,
+    },
+    on:{
       type: String,
       required: true,
     }
@@ -16,3 +58,31 @@ jogadoresSchema.plugin(normalize);
 var jogadores = mongoose.model('Jogadores', jogadoresSchema);
 
 module.exports = jogadores;
+{
+  "shots": {
+    "total": 0,
+    "on": 0
+  },
+  "goals": {
+    "total": 0,
+    "assists": 0
+  },
+  "passes": {
+    "total": 0,
+    "accuracy": 0
+  },
+  "duels": {
+    "total": 0,
+    "won": 0
+  },
+  "dribbles": {
+    "attempts": 0,
+    "success": 0
+  },
+  "fouls": {
+    "committed": 0
+  },
+  "cards": {
+    "yellow": 0,
+    "red": 0
+  }
--- backend/models/times.js
+++ backend/models/times.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const normalize = requise('normalize-mongoose');
 const timesSchema = new Schema({
-    nome:{
+    name:{
       type: String,
       required:true,
     },
-    sigla:{
+    logo:{
       type: String,
       required: true,
-    }
+    },league:{
+      type: String,
+      required:true,
+    },id:{
+      type: int,
+      required:true,
+    }, season:{
+      type: String,
+      required:true,
+    },
 })
 
 timesSchema.plugin(normalize);
