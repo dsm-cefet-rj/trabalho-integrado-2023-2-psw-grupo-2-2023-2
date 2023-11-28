98d89cdcacad68e12e559bb6fd6a3eb81e3844f0
FeixeDeLuz <luiz.mendescastro@gmail.com>
2023-11-28 09:41:39 -0300

    Esqueleto Backend Usuarios

--- backend/models/usuarios.js
+++ backend/models/usuarios.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
-const usuarios = mongoose.usuarios;
-
-const usuariosSchema = new Schema ({
-    
+const Schema = mongoose.Schema;
+const normalize = requise('normalize-mongoose');
+const usuariosSchema = new Schema({
+    nome:{
+      type: String,
+      required:true,
+    },
+    sigla:{
+      type: String,
+      required: true,
+    }
 })
+
+usuariosSchema.plugin(normalize);
+var usuarios = mongoose.model('Usuarios', timesSchema);
+
+module.exports = usuarios;
