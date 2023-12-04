c4ba0318bd045cdd573853fea456e1cbbd85900b
FilipeVelasco <filipeserravelasco@gmail.com>
2023-12-03 21:35:43 -0300

    conexão backend nos dados ja salvos

--- backend/models/times.js
+++ backend/models/times.js
@@ -29,3 +29,33 @@ timesSchema.plugin(normalize);
 var times = mongoose.model('Jogadores', timesSchema);
 
 module.exports = times;
+
+// AINDA SENDO FEITO
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Times = require('../models/times');
+
+router.post('/save-team', async (req, res) => {
+  try {
+    const { name, logo, league, id, season, jogadores } = req.body;
+
+    const newTeam = new Times({
+      name,
+      logo,
+      league,
+      id,
+      season,
+      jogadores,
+    });
+
+    const savedTeam = await newTeam.save();
+
+    res.status(201).json({ success: true, team: savedTeam });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
