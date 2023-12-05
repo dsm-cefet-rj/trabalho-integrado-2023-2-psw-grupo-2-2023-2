68c4034c48ae51b9832a9190a27fdfa7e636e04a
danielzanzao <daniel.mendes.1@aluno.cefet-rj.br>
2023-12-05 06:42:54 -0300

    CRUD minimamente feito

--- Backend2/routes/times.js
+++ Backend2/routes/times.js
@@ -24,13 +24,36 @@ router.route('/')
   res.json(times);
 
 })
-//rota com id especifico(usar pra usar um .edit possivelmente mais tarde)
-router.route('/:id')
-.delete((req, res, next) => {
-  res.params.get()
+.post((req, res, next) => {
+  let proxId = 1 + times.map(t=> t.id).reduce((x,y) =>Math.max(x,y));
+  let times = {...req.body, id: proxId}
+  times.push(times)
+
+  res.statusCode=200;
   res.setHeader('Content-Type', 'application/json');
   res.json(times);
 
-});
+})
+//rota com id especifico(usar pra usar um .edit possivelmente mais tarde)
+router.route('/:id')
+.delete((req, res, next) => {
+
+  times - times.filter(function(value,inder,arr){//usando splice e usando ja o jsonserver pra pegar os dados
+      return value.id != req.params.id;
+  });
+  
+  res.statusCode=200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(req.params.id);
+
+})
+.put((req, res, next) => {
+  let index = times.map(t=>t.id).indexOf(req.params.id);
+  times.splice(index,1,req.body);
+
+  res.statusCode=200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(req.body);
+  });
 
 module.exports = router;
